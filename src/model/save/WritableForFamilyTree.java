c78f03097874d3b7c21e2f3fbcba58f9a2682db3
Kostya <aktybaeff@yandex.ru>
2023-07-17 20:46:04 +0500

    change save and restore

--- src/model/save/WritableForFamilyTree.java
+++
@@ -1,9 +0,0 @@
-package model.save;
-
-import model.familyTree.FamilyTree;
-import model.human.Human;
-
-public interface WritableForFamilyTree {
-    boolean save(FamilyTree<Human> tree);
-    FamilyTree<Human> read();
-}
